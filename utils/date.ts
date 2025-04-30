f86ccd7283f5e75c56f0408fd7bc0afbb884c397
francofelicioni <fran.felicioni@gmail.com>
2025-04-30 20:27:07 +0200

    feat: add parseDateTime function to handle date and time parsing

--- utils/date.ts
+++ utils/date.ts
@@ -1,3 +1,7 @@
 export function formatDateToYYYYMMDD(date: Date) {
     return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`
 }
+
+export function parseDateTime(date: string, time: string): Date {
+    return new Date(`${date}T${time.length === 5 ? `${time}:00` : time}`)
+}
