c2b51f487a76cf9cb2ff4671e81054b0906645db
Franco Felicioni <felicioni@next-level.com>
2025-05-13 13:10:04 +0200

    fix: update background colors and improve button hover effects for consistency across components

--- components/header.tsx
+++ components/header.tsx
@@ -3,10 +3,24 @@ import Link from "next/link";
 import { LanguageSelector } from "./language-selector";
 import { MobileNav } from "./mobile-nav";
 import { Button } from "./ui/button";
+import { usePathname, useRouter } from "next/navigation";
 
 export function Header() {
     const { t } = useLanguage()
 
+    const router = useRouter();
+    const pathname = usePathname();
+
+    const handleScrollLink = (id: string) => (e: React.MouseEvent<HTMLAnchorElement>) => {
+        e.preventDefault();
+
+        if (pathname === "/") {
+            const section = document.getElementById(id);
+            if (section) section.scrollIntoView({ behavior: "smooth" });
+        } else {
+            router.push(`/#${id}`);
+        }
+    };
     return (
         <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60" >
             <div className="container flex h-16 items-center justify-between">
@@ -18,19 +32,20 @@ export function Header() {
                 </div>
                 <nav className="hidden md:flex items-center gap-6 lg:gap-12">
                     {["services", "about", "contact"].map((item) => (
-                        <Link
+                        <a
                             key={item}
-                            href={`#${item}`}
-                            className="text-md font-medium hover:text-blue-gray transition-colors duration-200"
+                            href={`/#${item}`}
+                            onClick={handleScrollLink(item)}
+                            className="text-md font-medium hover:text-blue-gray transition-colors duration-200 cursor-pointer"
                         >
                             {t(`nav.${item}`)}
-                        </Link>
+                        </a>
                     ))}
                 </nav>
                 <div className="flex items-center gap-2">
                     <LanguageSelector />
-                    <Button className="bg-button-orange hover:bg-legal-accent-dark hidden sm:inline-flex">
-                        <a href="#booking">{t("nav.bookCall")}</a>
+                    <Button className="bg-button-orange hover:bg-button-orange-hover hidden sm:inline-flex">
+                        <a onClick={handleScrollLink("booking")} href="/#booking">{t("nav.bookCall")}</a>
                     </Button>
                 </div>
             </div>
